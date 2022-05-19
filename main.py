8992622fb9c87dcd4e71018431335d354c5602c3
solucky-labs <solucky.cat@protonmail.com>
2022-05-19 21:37:23 +0200

    init

--- main.py
+++ main.py
@@ -1,29 +1,36 @@
 
-# create a file named token.py with token = 'your_oanda_token'
-import token 
+# create a file named key.py with token = 'your_oanda_token'
+from key import key
 
-
-from oandapyV20.contrib.factories import InstrumentsCandlesFactory
 from oandapyV20 import API
 import oandapyV20.endpoints.instruments as instruments
 import pandas as pd
 
-access_token = token.token
 
-client = API(access_token=access_token)
+client = API(access_token=key.token)
+
+
+periods = 3000
 
 params = {
     "granularity": 'H1',
-    "count": 3000
+    "count": periods
 }
 
 
-AUD = ['AUD_CAD','AUD_CHF','AUD_JPY','AUD_NZD']
+AUD = [ 'AUD_CAD','AUD_CHF','AUD_HKD','AUD_JPY','AUD_NZD','AUD_SGD','AUD_USD',
+        'CAD_CHF','CAD_HKD','CAP_JPY','CAD_SGD','CHF_HKD','CHF_JPY','EUR_AUD',
+        'EUR_CAD' ]
 
 
-r = instruments.InstrumentsCandles(instrument="EUR_USD",params=params)
 
-r = client.request(r)
 
-price = (float(r['candles'][0]['mid']['o'])+float(r['candles'][0]['mid']['h'])+float(r['candles'][0]['mid']['l'])+float(r['candles'][0]['mid']['c']))/4
-
+def index( currency, symbols ):
+    for sym in symbols:
+        for i in range(periods):
+            date = r['candles'][0]['time'][0:10]
+            hour = int(r['candles'][13]['time'][11:13])
+            price = (float(r['candles'][0]['mid']['o'])+float(r['candles'][0]['mid']['h'])+float(r['candles'][0]['mid']['l'])+float(r['candles'][0]['mid']['c']))/4
+            if inverse:
+                price = 1/price
+            price = round(price,6)
