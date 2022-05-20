b0976bc761c32c6f790d1aa88fa10336a49aae76
quantium-rock <alexcollsoutumuro@gmail.com>
2022-05-20 12:37:20 +0200

    index

--- db.py
+++ db.py
@@ -8,13 +8,6 @@ import pandas as pd
 
 client = API(access_token=key.token)
 
-periods = 5000
-
-params = {
-    'granularity': 'H1',
-    'count': periods
-}
-
 syms = ['AUD_CAD','AUD_CHF','AUD_HKD','AUD_JPY','AUD_NZD','AUD_SGD','AUD_USD',
         'CAD_CHF','CAD_HKD','CAD_JPY','CAD_SGD','CHF_HKD','CHF_JPY','EUR_AUD',
         'EUR_CAD','EUR_CHF','EUR_GBP','EUR_HKD','EUR_JPY','EUR_NZD','EUR_SGD',
@@ -26,7 +19,12 @@ syms = ['AUD_CAD','AUD_CHF','AUD_HKD','AUD_JPY','AUD_NZD','AUD_SGD','AUD_USD',
         'XAU_CAD','XAU_CHF','XAU_EUR','XAU_GBP','XAU_HKD','XAU_JPY','XAU_NZD',
         'XAU_SGD','XAU_USD','XAU_XAG']
 
-sym='AUD_CAD'
+periods = 5000
+
+params = {
+    'granularity': 'H1',
+    'count': periods
+}
 
 def db( symbols ):
     for sym in symbols:
@@ -35,15 +33,15 @@ def db( symbols ):
         r = client.request(r)
         df = pd.DataFrame()
         for i in range(len(r['candles'])):
-            index = r['candles'][i]['time'][0:19]
-            date = r['candles'][i]['time'][0:10]
-            hour = int(r['candles'][i]['time'][11:13])
-            price = round( ( float(r['candles'][i]['mid']['o'])
-                            +float(r['candles'][i]['mid']['h'])
-                            +float(r['candles'][i]['mid']['l'])
-                            +float(r['candles'][i]['mid']['c']))/4, 6 )
-            add = pd.DataFrame({'date': date, 'hour': hour, 'price': price }, index=[0])
+            date = r['candles'][i]['time'][0:19]
+            #date = r['candles'][i]['time'][0:10]
+            #hour = int(r['candles'][i]['time'][11:13])
+            ret = round((float(r['candles'][i]['mid']['c'])/float(r['candles'][i]['mid']['o'])-1)*100, 2)
+            #vol = round((float(r['candles'][i]['mid']['h'])/float(r['candles'][i]['mid']['l'])-1)*100, 2)
+            add = pd.DataFrame({'date': date, 'return': ret}, index=[0])
             df = pd.concat([df, add], ignore_index=True)
         df.to_csv('db/'+sym+'.csv', index=False)
         
 db(syms)
+
+
--- index.py
+++ index.py
@@ -1,10 +1,30 @@
 from os import listdir
-from os.path import isfile, join
+import pandas as pd
+
+def index( currency ):
+    print(currency+'...')
+    files = [k for k in listdir('db') if currency in k]
+    df = pd.read_csv('db/'+files[0])
+    if files[0][0:3] != currency: df['return'] = -df['return']
+    df = df.rename(columns={'return':files[0][0:7]})
+    for i in range(len(files)-1):
+        df2 = pd.read_csv('db/'+files[i+1])
+        if files[i+1][0:3] != currency: df2['return'] = -df2['return']
+        df2 = df2.rename(columns={'return':files[i+1][0:7]})
+        df = pd.merge(df,df2, on=['date','date'])
+    df = df.fillna(0)
+    df = df.set_index('date')
+    df[currency] = round(df.sum(axis=1),2)
+    df = df[currency]
+    df.to_csv('db/index/'+currency+'.csv', index=True)
 
 
+currencies = ['AUD','CAD','CHF','EUR','GBP','HKD','JPY','NZD','SGD','USD','XAG','XAU']
+
+for curr in currencies:
+    index(curr)
+        
+
+for file in currencies:
 
 
-
-files = [k for k in listdir('db') if 'AUD' in k]
-
-onlyfiles = [f for f in listdir('/db') if isfile(join('/db', f))]
