0a0ef34e556f382895a5a55938b59a3b1d791c2f
quantium-rock <alexcollsoutumuro@gmail.com>
2022-05-22 22:35:31 +0200

    bin

---
+++ bin/params.py
@@ -0,0 +1,13 @@
+
+GRANULARITY = 'D'
+LOOKBACK = 30
+
+# Lowpass filter parameters
+N = 2  # filter order
+Wn = 0.3  # cutoff frequency
+
+instruments = [
+    'AUD_CAD', 'AUD_CHF', 'AUD_HKD', 'AUD_JPY', 'AUD_NZD', 'AUD_SGD', 'AUD_USD', 'CAD_CHF', 'CAD_HKD', 'CAD_JPY', 'CAD_SGD', 'CHF_HKD', 'CHF_JPY', 'EUR_AUD',      'EUR_CAD', 'EUR_CHF', 'EUR_GBP', 'EUR_HKD', 'EUR_JPY', 'EUR_NZD', 'EUR_SGD', 'EUR_USD', 'GBP_AUD', 'GBP_CAD', 'GBP_CHF', 'GBP_HKD', 'GBP_JPY', 'GBP_NZD', 'GBP_SGD', 'GBP_USD', 'HKD_JPY', 'NZD_CAD', 'NZD_CHF', 'NZD_HKD', 'NZD_JPY', 'NZD_SGD', 'NZD_USD', 'SGD_CHF', 'SGD_HKD', 'SGD_JPY', 'USD_CAD', 'USD_CHF', 'USD_HKD', 'USD_JPY', 'USD_SGD']
+
+currencies = ['AUD', 'CAD', 'CHF', 'EUR',
+              'GBP', 'HKD', 'JPY', 'NZD', 'SGD', 'USD']
--- bin/system.py
+++ bin/system.py
@@ -3,9 +3,6 @@ from time import sleep
 from datetime import datetime
 import imp
 
-GRANULARITY = 'D'
-PERIODS = 100
-LOOKBACK = 30
 
 imp.load_source('db.py', GRANULARITY, str(LOOKBACK))
 
