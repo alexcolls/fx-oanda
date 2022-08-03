47feed384c812c726a87e0476ef174f7f53a44d4
quantium-rock <alexcollsoutumuro@gmail.com>
2022-08-03 21:29:58 +0200

    OandaApi v.0.2

--- model/oanda_api.py
+++ model/oanda_api.py
@@ -7,6 +7,9 @@ import requests
 import pandas as pd
 from pathlib import Path
 
+from datetime import datetime
+from datetime import timedelta
+
 # OANDA BROKER DATA-FEED API
 
 class OandaApi:
@@ -17,7 +20,7 @@ class OandaApi:
             "api": 'https://api-fxpractice.oanda.com'
         },
         "live": {
-            "stream": 'https://stream-fxtrade.oanda.com',
+            "stream": 'hstart_datettps://stream-fxtrade.oanda.com',
             "api": 'https://api-fxtrade.oanda.com'
         }
     }
@@ -30,40 +33,66 @@ class OandaApi:
 
         self.client = requests.Session()
         self.client.headers['Authorization'] = 'Bearer '+ TOKEN
-        # self.prices = self.Prices()
+        # self.prices = self.getPrices()
 
 
-    def Prices( self, symbols=__symbols__.SYMBOLS, timeframe='S5', from_year=2022, to_year=2022, version='v3' ):
+    def getYearsWeeks (  self, year=2017, symbols=__symbols__.SYMBOLS, api_version='v3' ):
 
-        for yr in range(int(from_year), int(to_year)+1):
-            
+        # get first monday of the year
+        first_monday = timedelta( days=( 7 - datetime.strptime(str(year), '%Y').weekday() ) )
+        first_monday = datetime.strptime(str(year), '%Y') + first_monday
+
+        # get last friday of the year
+        last_friday = timedelta(days=( 3 + datetime.strptime(str(year+1), '%Y').weekday()) )
+        last_friday = datetime.strptime(str(year+1), '%Y') - last_friday
+
+        mondays = [ first_monday + timedelta(weeks=i) for i in range(51) ]
+
+        for i, monday in enumerate(mondays):
+            if monday.weekday() == 0:
+                mondays[i] = datetime.strftime(monday, '%Y-%m-%d') + 'T00:00:00.000000000Z'
+            else:
+                print('Its not a Monday!', i)
+
+        wk = 0
+        
+        for monday in mondays:
+
+            wk += 1
+
+            asks = pd.DataFrame()
+            bids = pd.DataFrame()
+
             for symbol in symbols:
 
-                print(symbol)
+                print(symbol, 'week ', wk)
 
-                data = { 'dt': [], 'ask': [], 'bid': [], 'vol': [] }
+                data = { 'dt': [], 'ask': [], 'bid': [] }
 
-                start_date = str(yr)+'-01-01'
-
+                start_date = monday
+                friday = False
                 iterate = True
 
                 while iterate:
 
-                    req = self.client.get(f'{self.enviroment}/{version}/instruments/{symbol}/candles?count=5000&price=BA&granularity={timeframe}&from={start_date}')
-
+                    # request 5 second candles from oanda rest-api
+                    req = self.client.get(f'{self.enviroment}/{api_version}/instruments/{symbol}/candles?count=5000&price=BA&granularity=S5&from={start_date}&includeFirst=False')
                     req = json.loads(req.content.decode('utf-8'))['candles']
 
+                    print(start_date[:19], req[-1]['time'][:19])
+
+                    if not friday and datetime.strptime(req[0]['time'][:19], '%Y-%m-%dT%H:%M:%S').weekday() == 4:
+                        friday = True
+
                     digits = len(req[0]['ask']['c'].split('.')[1])
-                    year = req[0]['time'][:4]
 
                     for x in req:
-                        year = x['time'][:4]
-                        if year != str(yr):
+                        
+                        if friday and datetime.strptime(x['time'][:19], '%Y-%m-%dT%H:%M:%S').weekday() != 4:
                             iterate = False
                             break
 
                         data['dt'].append(x['time'])
-                        data['vol'].append(int(x['volume']))
                         for p in ['ask', 'bid']:
                             o = float(x[p]['o'])
                             h = float(x[p]['h'])
@@ -72,22 +101,35 @@ class OandaApi:
                             data[p].append( round((o+h+l+c*2)/5, digits) )
                         
                     if start_date == data['dt'][-1]:
+                        iterate = False
                         del req
                         break
 
                     start_date = data['dt'][-1]
+
+                _asks = pd.DataFrame(data['ask'], index=data['dt'], columns=[symbol])
+                _asks.index = pd.to_datetime(_asks.index)
+                _bids = pd.DataFrame(data['bid'], index=data['dt'], columns=[symbol])
+                _bids.index = pd.to_datetime(_bids.index)
+
+                if len(asks) == 0:
+                    asks = _asks
+                    bids = _bids
+                else:
+                    asks = pd.merge(asks, _asks, how='inner', left_index=True, right_index=True)
+                    bids = pd.merge(bids, _bids, how='inner', left_index=True, right_index=True)
+
+                print(asks)
+
+                del data, _asks, _bids
+
+            return asks
+
+            
+            
+            
         
-                data = pd.DataFrame.from_dict(data)
-                data.dt = pd.to_datetime(data.dt)
-                data = data.set_index('dt')
-                data = data[~data.index.duplicated(keep='first')]        
-                
-                path = 'db/prices/'+str(yr)
-                Path(path).mkdir(parents=True, exist_ok=True)
-                data.to_csv(path+symbol+'.csv', index=True)
-
-                del data
-
-
 
 oanda = OandaApi()
+
+
